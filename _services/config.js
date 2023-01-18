3994db9c5175d6c24e4d6328daeed0679c8bdad5
Davide Baldelli <davbaldelli@gmail.com>
2023-01-18 19:21:42 +0100

    added local url with ssl

--- _services/config.js
+++ _services/config.js
@@ -1,2 +1,2 @@
-// export const API_URL = 'http://localhost:7151/api'
+// export const API_URL = 'https://home.davidebaldelli.it:7151/api'
 export const API_URL = 'https://spotter.davidebaldelli.it:7151/api'
