bd2b604a87ba42899c64759fe5e316c45aa904a4
Davide Baldelli <davbaldelli@gmail.com>
2023-01-19 13:27:46 +0100

    fixed session not sorting on mobile

--- _helpers/session-sorters.js
+++ _helpers/session-sorters.js
@@ -1,7 +1,9 @@
+import moment from "moment";
+
 const sortByDateTime = (a,b) => {
-  const dateTimeA = `${a.date} ${a.time}`
-  const dateTimeB = `${b.date} ${b.time}`
-  return dateTimeA > dateTimeB
+  const dateTimeA = moment(`${a.date} ${a.time}`)
+  const dateTimeB = moment(`${b.date} ${b.time}`)
+  return dateTimeA.isAfter(dateTimeB)
 }
 export const sessionSorters = {
   sortByDateTime
