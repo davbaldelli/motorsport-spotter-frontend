3a42d91cf566a3eefae628cf52ddec6829fdc81d
dbell <davide.baldelli@nonstudio.org>
2025-03-28 19:37:46 +0100

    changed remote

--- _services/config.js
+++ _services/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://api.motorsportspotter.com/api'
+export const API_URL = 'https://dev.api.motorsportspotter.com/api'
 // export const API_URL = 'http://localhost:7151/api'
